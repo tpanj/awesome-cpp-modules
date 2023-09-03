6d40e2d0a481fc0186382c6d6834876716b70126
Tadej Panjtar <tadej.panjtar@gmail.com>
2023-09-03 12:47:08 +0200

    Extend example with function

--- foo.cc
+++ foo.cc
@@ -4,15 +4,33 @@ module;
 
 export module foo;
 
+// Forwad declaration of function is not needed
+// baceuse e.g. it was not used in cosntrucor in class declaration
+// but if also want to hold signatures in one place..
+// export int publicFunction(int a, int b);
+
+// Whole class export
 export class foo {
 public:
-  foo();
+  foo() = default;
   ~foo();
-  void helloworld();
+  void helloworld(int n);
 };
 
-foo::foo() = default;
-foo::~foo() = default;
-void foo::helloworld() { 
-	std::cout << "hello world\n";
+// It can not be done in class declaration because of std:: import use
+foo::~foo() {
+    std::cout << "Destructor called \n";
+}
+
+// Method example
+void foo::helloworld(int n) {
+  std::cout << "hello world " << n << std::endl;
+}
+
+int privateFunction(int a, int b) {
+  return a * b;
+}
+
+export int publicFunction(int a, int b) {
+  return privateFunction(a, b);
 }
--- main.cc
+++ main.cc
@@ -2,7 +2,10 @@ import foo;
 
 int main()
 {
+  // Class creation and constructor call
   foo f;
-  f.helloworld();
+
+  // Using exported function output as agument to method
+  f.helloworld(publicFunction(6, 7) + publicFunction(5, 7));
   return 0;
 }
